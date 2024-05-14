a7e189f7e66da1160a237488245828be733bc3c2
NguyenMinhThanB2014519 <htna705@gmail.com>
2024-05-14 23:20:43 +0700

    Modify server configuration

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__=='__main__':
-    uvicorn.run(app='app.app:app', host='0.0.0.0', port=81, reload=True)
+    uvicorn.run(app='app.app:app', host='127.0.0.1', port=81, reload=True)
