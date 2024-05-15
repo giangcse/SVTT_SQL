2d6a15feaf521b3f617f671f88f68fe31e9aa2f4
NguyenMinhThanB2014519 <htna705@gmail.com>
2024-05-15 17:19:50 +0700

    modify admin's username & password

--- create_passwd.py
+++ create_passwd.py
@@ -1,8 +1,11 @@
 from hashlib import sha3_256
-from .app.config import secret_key, algorithm
+from app.config import secret_key, algorithm
 
 SECRET_KEY = secret_key
 ALGORITHM = algorithm
 
 passwd = input("Type password: ")
 print(sha3_256(bytes(passwd, 'utf-8')).hexdigest())
+
+# 28dac06a86df34adba461d94e4181b8acc21d292a0df7b14574d4467bff33644
+# pssd:  admin@131299  username: adminThan;
