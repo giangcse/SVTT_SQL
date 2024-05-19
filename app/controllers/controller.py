5b17328a6b2c25c4b18ae1acd4a9583becfda062
Dothuan6 <thuann6222@gmail.com>
2024-05-19 20:08:08 +0700

    them chuc nang xuat phieu

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -380,6 +380,8 @@ def ctu_xuat_phieu_tiep_nhan_controller(sv_id: int):
 def ctu_xuat_phieu_giao_viec_controller(sv_id: int, username: str):
     return ctu_xuat_phieu_giao_viec_model(sv_id, username)
 
-
 def ctu_xuat_phieu_theo_doi_controller(sv_id: int, username: str):
     return ctu_xuat_phieu_giao_viec_model(sv_id, username)
+# chuc nang xuat danh gia
+def ctu_xuat_phieu_danh_gia_controller(sv_id: int,username:str):
+    return ctu_xuat_phieu_danh_gia_model(sv_id,username)
