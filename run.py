5b28dca21d55a8d3512530a97ef42ad621ca552f
giangcse <giangphan028@gmail.com>
2024-07-22 18:37:32 +0700

    Updated: credit

--- run.py
+++ run.py
@@ -7,11 +7,11 @@ def kill_telegram_process():
     os.system('taskkill /f /im python.exe')
 
 if __name__ == '__main__':
-    # Chạy file telegram_reply.py trong một CMD mới và ngầm
-    subprocess.Popen('start /b python telegram_reply.py', shell=True)
+    # # Chạy file telegram_reply.py trong một CMD mới và ngầm
+    # subprocess.Popen('start /b python telegram_reply.py', shell=True)
 
-    # Đăng ký hàm để kill process khi kết thúc
-    atexit.register(kill_telegram_process)
+    # # Đăng ký hàm để kill process khi kết thúc
+    # atexit.register(kill_telegram_process)
 
     # Chạy Uvicorn server
-    uvicorn.run(app='app.app:app', host='0.0.0.0', port=81, reload=True)
+    uvicorn.run(app='app.app:app', host='0.0.0.0', port=8000, reload=True)
